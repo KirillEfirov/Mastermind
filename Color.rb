68726159536750d969ce22add1338200977e693a
Kirill <shishkokirill.fudosin@gmail.com>
2022-11-15 07:13:21 +0300

    Make function that can generate color depending on the given number

---
+++ Color.rb
@@ -0,0 +1,18 @@
+module Color
+    def apply_color(mytext)
+        case mytext
+        when "1"
+            "\e[41m#{mytext}\e[0m"   
+        when "2"
+            "\e[42m#{mytext}\e[0m"
+        when "3"
+            "\e[43m#{mytext}\e[0m"
+        when "4"
+            "\e[44m#{mytext}\e[0m"
+        when "5"
+            "\e[45m#{mytext}\e[0m"
+        when "6"
+            "\e[46m#{mytext}\e[0m"
+        end
+    end
+end
