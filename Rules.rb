7add73ff53f93d398558aa640b892f3b338e2611
Kirill <shishkokirill.fudosin@gmail.com>
2022-11-15 07:51:00 +0300

    Make rules for player

---
+++ Rules.rb
@@ -0,0 +1,8 @@
+class Rules
+    def print_rules
+        puts "Player is given 12 attempts quess the code that computer generated at random"
+        puts "After every attempt player gets a hint: "
+        puts "#{"\u2713".encode('utf-8')} - one of the numbers is in the correct position"
+        puts "#{"\u2721".encode('utf-8')} - there's the right number but on the wrong position"
+    end
+end
