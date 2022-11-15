1164c6bc42b191493aa3d2542262731567db88ce
Kirill <shishkokirill.fudosin@gmail.com>
2022-11-15 07:15:26 +0300

    Include module Color and make function that can display code with back-ground color

--- Computer.rb
+++ Computer.rb
@@ -1,4 +1,8 @@
+require_relative 'Color.rb'
+
 class Computer
+    include Color
+
     @@code = Array.new
 
     def generate_code
@@ -7,5 +11,16 @@ class Computer
 
     def get_code
         @@code
+    end
+
+    def display_code
+        @@code.each do |elem|
+            print apply_color(elem.to_s) + " "
+        end
+        puts
+    end
+
+    def show_clue
+        
     end
 end
