f1d29366dfd8eff8ad91f478bb6a11dbea9827da
Kirill <shishkokirill.fudosin@gmail.com>
2022-11-17 18:37:14 +0300

    Player can guess the code

--- Player.rb
+++ Player.rb
@@ -1,2 +1,13 @@
 class Player
+    attr_accessor :user_code
+
+    def guess
+        print "Enter a code: "
+        @user_code = gets.chomp!
+    end
+
 end
+
+#user = Player.new
+#user.guess
+#puts user.user_code
