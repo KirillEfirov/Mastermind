62e01d4d5ba5eb837ea21768584b189b1dfb5cd5
Kirill <shishkokirill.fudosin@gmail.com>
2022-11-24 06:35:20 +0300

    Apply color on 'RULES' word

--- Rules.rb
+++ Rules.rb
@@ -1,6 +1,10 @@
+require_relative 'Color.rb'
+
 class Rules
+    include Color
     def print_rules
-        puts "Player is given 12 attempts quess the code that computer generated at random"
+        puts "#{apply_color("RULES")}"
+        puts "Player is given 12 attempts to guess the code that computer generated at random"
         puts "After every attempt player gets a hint: "
         puts "#{"\u2713".encode('utf-8')} - one of the numbers is in the correct position"
         puts "#{"\u2721".encode('utf-8')} - there's the right number but on the wrong position"
